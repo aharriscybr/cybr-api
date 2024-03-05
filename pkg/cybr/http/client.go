d62bca55e015b61f51aeb2461e0c1c93ec3df33d
Andrew Harris <andrew.harris@cyberark.com>
2024-03-05 16:45:56 -0500

    hashi api refactoring

--- pkg/cybr/http/client.go
+++ pkg/cybr/http/client.go
@@ -1,20 +1,61 @@
-package hClient
+package cyberark
 
 import (
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/aharriscybr/cybr-api/pkg/cybr/auth"
+	"github.com/aharriscybr/cybr-api/pkg/cybr/types"
 )
 
+var (
+	api_uri string
+)
+
+type Client struct {
+	URI string
+	HTTPClient *http.Client
+	Auth string
+}
+
 // Return configured HTTP Client
-func GetClient() *http.Client {
+func GetClient(conf *types.CloudConfig) (*Client, error) {
 
-	c := &http.Client{
+	if conf.Tenant == "" || conf.ClientID == "" || conf.ClientSecret == "" || conf.Domain == "" || conf.Action == "" {
 
-		Transport: &http.Transport{
-			TLSHandshakeTimeout: 700 * time.Millisecond,
-		},
-	}
+		return nil, nil
+
+	} else {
+
+		if conf.Action == "auth" {
+			api_uri = "https://" + conf.Tenant + ".id.cyberark.cloud/oauth2/platformtoken"
+
+			token, result, err := auth.GetIdentityToken(conf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if result {
+				c := Client {
+					HTTPClient: &http.Client{Timeout: 10 * time.Second},
+					URI: api_uri,
+					Auth: token,
+				}
+				return &c, nil
+			}
+
+		}
+		// TODO Add onboarding client information
+		if conf.Action == "create" {
+			api_uri = "https://" + conf.Domain + ".privilegecloud.cyberark.cloud/PasswordVault/API/Accounts"
+			c := Client {
+				HTTPClient: &http.Client{Timeout: 10 * time.Second},
+				URI: api_uri,
+				Auth: "blank",
+			}	
+			return &c, nil
+		}
+		
 	
-	return c
-
+	}
 }
